fa142d46904085bdf2f90b315d979fdb50216f66
ishayashika <yashikakumari33@gmail.com>
2026-08-03 04:51:13 +0530

    Redirect home page to register

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">
-        Personal Book Manager 📚
-      </h1>
-    </main>
-  );
+  redirect("/register");
 }
